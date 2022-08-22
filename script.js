8b91f7126aa159453ca2342c9fdd12ace0f0287c
shervin <shervin.ses@gmail.com>
2022-08-22 16:32:58 +0430

    menu-bar

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ let searchBar = document.querySelector(".search-bar-container");
 let formBtn = document.querySelector("#login-btn");
 let loginForm = document.querySelector(".login-form-container");
 let formClose = document.querySelector("#form-close");
+let menu = document.querySelector("#menu-bar");
+let navBar = document.querySelector(".navbar");
 
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
+  menu.classList.remove("fa-times");
+  navBar.classList.remove("active");
+  loginForm.classList.remove("active");
 };
 
 searchBtn.addEventListener("click", () => {
   searchBtn.classList.toggle("fa-times");
   searchBar.classList.toggle("active");
 });
+menu.addEventListener("click", () => {
+    menu.classList.toggle("fa-times");
+    navBar.classList.toggle("active");
+  });
 formBtn.addEventListener("click", () => {
   loginForm.classList.add("active");
 });
