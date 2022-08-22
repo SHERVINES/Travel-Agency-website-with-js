2dba2673323e5e45bc883d4eb15c1ee5836ce467
shervin <shervin.ses@gmail.com>
2022-08-22 19:29:31 +0430

    main section

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let loginForm = document.querySelector(".login-form-container");
 let formClose = document.querySelector("#form-close");
 let menu = document.querySelector("#menu-bar");
 let navBar = document.querySelector(".navbar");
+let videoBtn = document.querySelectorAll(".video-btn");
 
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
@@ -19,12 +20,21 @@ searchBtn.addEventListener("click", () => {
   searchBar.classList.toggle("active");
 });
 menu.addEventListener("click", () => {
-    menu.classList.toggle("fa-times");
-    navBar.classList.toggle("active");
-  });
+  menu.classList.toggle("fa-times");
+  navBar.classList.toggle("active");
+});
 formBtn.addEventListener("click", () => {
   loginForm.classList.add("active");
 });
 formClose.addEventListener("click", () => {
   loginForm.classList.remove("active");
+});
+
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src =src;
+});
 });
