fb554e0c446fccc6f620a45c648572ab58aa568f
shervin <shervin.ses@gmail.com>
2022-08-23 21:44:53 +0430

    review section

--- script.js
+++ script.js
@@ -38,3 +38,22 @@ videoBtn.forEach((btn) => {
     document.querySelector("#video-slider").src = src;
   });
 });
+let swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
