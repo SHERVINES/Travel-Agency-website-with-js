bd255106715114245293aaaa6634ae8c442d5aab
shervin <shervin.ses@gmail.com>
2022-08-24 13:28:50 +0430

    brand section

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ videoBtn.forEach((btn) => {
     document.querySelector("#video-slider").src = src;
   });
 });
-let swiper = new Swiper(".review-slider", {
+var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   loop: true,
   autoplay: {
@@ -54,6 +54,28 @@ let swiper = new Swiper(".review-slider", {
     },
     1024: {
       slidesPerView: 3,
+    },
+  },
+});
+var swiper = new Swiper(".brand-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
     },
   },
 });
